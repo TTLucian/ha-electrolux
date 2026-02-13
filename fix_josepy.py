1f54f5584b87912abfda767e940043909614ff08
TTLucian <1855276+TTLucian@users.noreply.github.com>
2026-02-13 14:37:52 +0200

    updates

--- fix_josepy.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""Monkey patch josepy to add ComparableX509 for acme compatibility."""
-import josepy
-
-# Add ComparableX509 as an alias for ComparableKey to fix acme compatibility
-josepy.ComparableX509 = josepy.ComparableKey  # type: ignore
